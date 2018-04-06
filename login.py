e500fbfb865e440b27930ce75e6ba9c5d1f0aa1e
Brandon Marco <36774208+bmarco1@users.noreply.github.com>
2018-04-06 11:20:33 -0400

    Add files via upload

---
+++ login.py
@@ -0,0 +1,12 @@
+print("input a rate and a diatance")
+rate = float(input("Rate: "))
+distance = float(input("Distance: "))
+print("time:", (distance / rate))
+
+
+
+password = str()
+
+while password != "unicorn":
+    password = input("Password: ")
+Print("Welcome in")
